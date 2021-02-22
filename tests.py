efda3af57046aa9c9d8d400b64f03f310ec12b44
Jayson Rock <rock@uwm.edu>
2021-02-22 16:11:31 -0600

    Initial commit

---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestStringMethod_Upper(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+class TestStringMethod_Split(unittest.TestCase):
+    def test_twoWords(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+
+    def test_splitInt(self):
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s = 'hello world'
+            s.split(":")
